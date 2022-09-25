e8b5348cd2eb2cf72f93ab5e2d511d14d8c7a551
datagutten <datagutten@datagutten.net>
2022-09-25 14:48:27 +0200

    Multi version package

--- get_libs.sh
+++ get_libs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g 1.13.6 -c -d -z
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g wrath -c -d -z
 mv .release/TomTomTargetArrow/libs .
