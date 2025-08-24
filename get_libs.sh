023d4cbff4cca5f7fdf537509c393ed0bfab90d6
Anders Birkenes <datagutten@datagutten.net>
2025-08-24 20:09:12 +0200

    Update interface versions

--- get_libs.sh
+++ get_libs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g wrath -c -d -z
-mv .release/TomTomTargetArrow/libs .
+rsync -av --del .release/TomTomTargetArrow/libs .
